cf465136efab4fa5595b08d4e5fdbe27d869031a
Feyza <129684598+feeyza@users.noreply.github.com>
2024-12-07 13:12:25 +0300

    Create a.php

---
+++ a.php
@@ -0,0 +1,19 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+
+
+  <p>Hello World</p>
+
+    
+    <?php
+      for ($a=0; $a<100; $a++) {
+        echo '<p> Hello world </p>';
+      }
+    ?>
+
+    
+  </body>
+</html>
